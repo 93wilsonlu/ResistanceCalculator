18081242781abda2fbf47930a7a8e909bf341164
93wilsonlu <93wilsonlu@gmail.com>
2021-03-17 11:22:48 +0800

    Update website title

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 
+document.title = "電阻計算器";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
